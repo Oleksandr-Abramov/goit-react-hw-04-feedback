120aea925ac415796ecf9ac57e43654633b2e2df
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-07-28 15:10:32 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,100 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Notification } from './Notification/Notification';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 
-export class App extends Component {
-  state = { good: 0, neutral: 0, bad: 0 };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handlerFeedback = evt => {
+  const handlerFeedback = evt => {
     const { name } = evt.target;
-    this.setState(prevState => {
-      return { [name]: prevState[name] + 1 };
-    });
+    switch (name) {
+      case 'good':
+        setGood(ps => ps + 1);
+        break;
+      case 'neutral':
+        setNeutral(ps => ps + 1);
+        break;
+      case 'bad':
+        setBad(ps => ps + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  render() {
-    const { good } = this.state;
-    const { neutral } = this.state;
-    const { bad } = this.state;
-    const stateKeys = Object.keys(this.state);
-    const total = good + neutral + bad;
-    const positiveFeedback = Math.round((good * 100) / total);
+  const stateKeys = ['good', 'neutral', 'bad'];
+  const total = () => good + neutral + bad;
+  const positiveFeedback = () => Math.round((good * 100) / total());
 
-    return (
-      <div style={{ padding: '30px' }}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            handlerFeedback={this.handlerFeedback}
-            options={stateKeys}
+  return (
+    <div style={{ padding: '30px' }}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          handlerFeedback={handlerFeedback}
+          options={stateKeys}
+        />
+      </Section>
+      {total() ? (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total()}
+            positiveFeedback={positiveFeedback()}
           />
         </Section>
-        {total ? (
-          <Section title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positiveFeedback={positiveFeedback}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
+};
+
+// export class App extends Component {
+//   state = { good: 0, neutral: 0, bad: 0 };
+
+//   handlerFeedback = evt => {
+//     const { name } = evt.target;
+//     this.setState(prevState => {
+//       return { [name]: prevState[name] + 1 };
+//     });
+//   };
+
+//   render() {
+//     const { good } = this.state;
+//     const { neutral } = this.state;
+//     const { bad } = this.state;
+//     const stateKeys = Object.keys(this.state);
+//     const total = good + neutral + bad;
+//     const positiveFeedback = Math.round((good * 100) / total);
+
+//     return (
+//       <div style={{ padding: '30px' }}>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             handlerFeedback={this.handlerFeedback}
+//             options={stateKeys}
+//           />
+//         </Section>
+//         {total ? (
+//           <Section title="Statistics">
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               positiveFeedback={positiveFeedback}
+//             />
+//           </Section>
+//         ) : (
+//           <Notification message="There is no feedback" />
+//         )}
+//       </div>
+//     );
+//   }
+// }
